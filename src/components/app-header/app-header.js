88640448938f669e7f7b66c36a53b73a5543e5be
bounderoll <bounderoll.23@gmail.com>
2021-08-28 19:50:29 +0400

    first-commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css'
+
+const AppHeader = ({liked, allPosts}) => {
+	return (
+		<div className="app-header d-flex">
+			<h1>Banenkov Maxim</h1>
+			<h2>{allPosts}, из них понравилось {liked}</h2>
+		</div>
+	)
+}
+
+export default AppHeader;
